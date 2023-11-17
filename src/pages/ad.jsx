b3cff6bc00cdb630705687ca7cad7e2decc02fe1
SHIVANSHU-TECH <Shivanshushuklacse@delhitechnicalcampus.ac.in>
2023-11-17 10:03:39 -0800

    everything is done

---
+++ src/pages/ad.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from "../components/Form/form";
+import Header from '../components/Header/Header';
+function ad() {
+  return (
+    <div>
+      <Header />
+      {/* <h1>hello</h1> */}
+      <Form /> 
+    </div>
+  );
+}
+
+export default ad;
